ed9deb4954d051cd73502198817dae16783816c2
Caitlin Hines and Elise St Hilaire <caitlin+elise@>
2017-02-28 16:39:01 -0800

    add students.cshtml and path on homemodule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,39 @@ namespace University
                 List<Department> AllDepartments = Department.GetAll();
                 return View["index.cshtml", AllDepartments];
             };
+
+            Post["/department/new"] = _ => {
+                Department newDepartment = new Department(Request.Form["department-name"]);
+                newDepartment.Save();
+
+                List<Department> AllDepartments = Department.GetAll();
+                return View["index.cshtml", AllDepartments];
+            };
+
+            Get["/courses"] = _ => {
+                List<Course> AllCourses = Course.GetAll();
+                return View["courses.cshtml", AllCourses];
+            };
+
+            Post["/course/new"] = _ => {
+                Course newCourse = new Course(Request.Form["course-name"], Request.Form["course-number"]);
+                newCourse.Save();
+                List<Course> AllCourses = Course.GetAll();
+                return View["courses.cshtml", AllCourses];
+            };
+
+            Get["/students"] = _ => {
+                List<Student> AllStudents = Student.GetAll();
+                return View["students.cshtml", AllStudents];
+            };
+
+            Post["/student/new"] = _ => {
+                Student newStudent = new Student(Request.Form["student-name"]);
+                newStudent.Save();
+                List<Student> AllStudents = Student.GetAll();
+                return View["students.cshtml", AllStudents];
+            };
+
         }
     }
 }
